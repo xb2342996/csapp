5562b2af204d695c0da32fe13424c73635617b81
Biao Xiong <536523401@qq.com>
2022-01-19 00:35:41 +0800

    movq指令

--- instruction.py
+++ instruction.py
@@ -4,6 +4,7 @@
 """
 
 from components import Register
+import re
 
 class Instruction:
     def __init__(self, ins, src, dest=None):
@@ -30,7 +31,26 @@ class InstructionDecoder:
            dest_code = Register.get_id(dest.replace('%', ''))
            return 0x20 << 8 | src_code << 4 | dest_code
 
+        if ins == 'irmovq':
+            src_code = int(src.replace('$', ''))
+            dest_code = Register.get_id(dest.replace('%', ''))
+            return 0x30 << 64 | 0xF << 60 | dest_code << 56 | src_code
 
-ins = Instruction('rrmovq', '%rax', '%rbx')
+        if ins == 'rmmovq':
+            src_code = Register.get_id(src.replace('%', ''))
+            imm, dest_code = re.match(r'(.*)\((.*)\)', dest).groups()
+            dest_code = Register.get_id(dest_code.replace('%', ''))
+            return 0x40 << 64 | src_code << 60 | dest_code << 56 | int(imm, 16)
+
+        if ins == 'mrmovq':
+            imm, src_code = re.match(r'(.*)\((.*)\)', src).groups()
+            src_code = Register.get_id(src_code.replace('%', ''))
+            dest_code = Register.get_id(dest.replace('%', ''))
+            return 0x50 << 64 | src_code << 60 | dest_code << 56 | int(imm, 16)
+
+# ins = Instruction('rrmovq', '%rax', '%rbx')
+# ins = Instruction('irmovq', '$15', '%rbx')
+# ins = Instruction('rmmovq', '%rsp', '0x123456789abcd(%rdx)')
+ins = Instruction('mrmovq',  '0x123456789abcd(%rdx)', '%rsp')
 decoder = InstructionDecoder()
-print(decoder.parse(ins))
+print(hex(decoder.parse(ins)))
